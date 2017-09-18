26a2c141f5edb883ffec2fa0614b057c48469b23
Blaine Arnau <Blaine.Arnau@tritech.com>
2017-09-17 19:15:42 -0700

    Pre-loadingwith resolver

---
+++ src/app/events/events-list/events-list-resolver.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { EventService } from '../shared/event.service';
+
+@Injectable()
+export class EventsListResolverService implements Resolve<any> {
+  
+  constructor(private eventService: EventService) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    this.eventService.getEvents().map(events => events) //this is short hand for ->
+    //typically when you listen to an observable you call .subscribe() instead of .map()
+    //but because this is in a resolver we need to actually return the observable instead of subscription
+      //this.eventService.getEvents().map((events) => { return events; })
+  }
+
+
+}
